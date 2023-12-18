848fb195e70324c215e7b06151789ed0e5eee6ab
wguadalupe <w.guadalupe78@gmail.com>
2023-12-17 20:10:36 -0500

    Fixed errors in my functions. Added notes to README

--- README.md
+++ README.md
@@ -4,3 +4,35 @@
 Images by: 
 Maygonpepetreynsh https://opengameart.org/users/maygonpepetreynsh
 Behnam-Norouzi: https://unsplash.com/@behy_studio
+
+### JavaScript Logic
+1. **Global Variables**:
+   - `memBoardSize`: Specifies the board size (16 tiles).
+   - `memoryBoard`: Array to store board elements (tiles).
+   - `firstTile`, `secondTile`: Variables to track the first and second tiles chosen by the player.
+   - `score`: Tracks the player's score.
+
+2. **`newBoard` Function**:
+   - Populates `memoryBoard` with pairs of identical values (up to half of `memBoardSize`).
+   - Shuffles the board using a sort function with a random comparator.
+
+3. **`resetBoard` Function**:
+   - Resets `memoryBoard`, `firstTile`, `secondTile`, and `score`.
+   - Updates the score display to '0'.
+   - Calls `newBoard` and `rendernewBoard` functions to reinitialize the game.
+
+4. **`rendernewBoard` Function**:
+   - Creates and displays each tile as a `div` element.
+   - Sets up a click event listener for each tile to handle tile flipping and score updates.
+   - If two tiles match, the score is incremented; otherwise, the tiles are flipped back after 1 second.
+
+5. **Event Listener for 'Shuffle' Button**:
+   - Calls `resetBoard` function when the 'Shuffle tiles' button is clicked.
+
+### HTML Structure
+- Contains a score section, a shuffle button, a timer display, and a `div` for the game board.
+
+### CSS Styling
+- Uses Google Font 'Inconsolata'.
+- Sets basic styles for body and elements like the game board and tiles.
+- Includes styling for flipped tiles and other visual aspects like the background image.
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function newBoard() {
     //this loop will run until i is less than half of boardSize
 for (let i = 0; i < memBoardSize /2; i++) { 
     memoryBoard.push(i);//this adds the current value of 'i' to the end of the memoryBoard array
-    memBoardSize.push(i);//since this needs to be done twice for the memory game, it adds 2 identical values each time. 
+    memoryBoard.push(i);//since this needs to be done twice for the memory game, it adds 2 identical values each time. 
 }
 
 //shuffle the board
@@ -16,11 +16,12 @@ memoryBoard = memoryBoard.sort(() => Math.random() - 0.5);//memoryBoard.sort is 
                                                          //the sorting function () => Math.random() - 0.5 is a funtion that returns a random number that is less than, equal to, or greater than 0.5
 }
 
+//reset board function
 function resetBoard() {
     memoryBoard = [];
-    let firstTile = null;
-    let secondTile = null; 
-    let score = 0;
+    firstTile = null;
+    secondTile = null; 
+    score = 0;
     document.getElementById('score').innerText = 'Score: 0'; //selects the ID 'score' then sets its innerText property to 0
     newBoard();
     rendernewBoard();
@@ -32,40 +33,43 @@ function rendernewBoard() {
     memoryGameBoard.innerHTML = ''; //resets the board
     //iterates over the memoryBoard array
     for (let i = 0; i < memoryBoard.length; i++) {
-    const tileElement = document.createElement('div'); //creates a new div for each tile
-    tileElement.classList.add('tile');//adds a class of 'tile' for styling
+    const tileElement = document.createElement('div'); //creates a new div for each tile.
+    tileElement.classList.add('tile');//adds a class of 'tile' for styling.
     tileElement.dataset.value = memoryBoard[i]; //Each tile's value (from the memoryBoard array) is stored in a custom data attribute (data-value)
     tileElement.innerText = '?';// Initially, each tile displays a '?' character.
     
-    //Appends an event listener for events whose type attribute value is type.
-    tileElement.addEventListener('click', () =>{
-        if (!firstTile) {
+    //responds when a tile is clicked
+    tileElement.addEventListener('click', () => {
+        if (!firstTile) { //if 'firstTile' is not already set the clicked tile becomes the firstTile.
             firstTile = tileElement;
-            tileElement.classList.add('flipped');
+            tileElement.classList.add('flipped'); //shows the tile's value
             tileElement.innerText = tileElement.dataset.value;
-        } else if (!secondTile) {
+        } else if (!secondTile && tileElement !== firstTile) { //else if 'secondTile' is not set and the clicked tile is not the same as 'firstTile', the ckicked tile becomes the sercondTile.
             secondTile = tileElement;
             tileElement.classList.add('flipped');
-            tileElement.innerText = tileElement.dataset.value) {
+            tileElement.innerText = tileElement.dataset.value;
+            //if firstTile and the clicked tile ('secondTile') have the same 'dataset.value', the score is incremented.
+            if (firstTile.dataset.value === tileElement.dataset.value) {
                 score++;
-                document.getElementById('score').innerText = 'Score: ' + score;
+                document.getElementById('score').innerText = 'Score: ' + score; //the score is updated and bot 'firstTile' and 'secondTile' is set to null.
                 firstTile = null;
                 secondTile = null;
             } else {
+                //if the tiles don't match, a 'setTimeout' is used to flip the tiles back over after 1 second
                 setTimeout(() => {
                     firstTile.classList.remove('flipped');
                     secondTile.classList.remove('flipped');
                     firstTile = null;
                     secondTile = null;
                 }, 1000);
-            }
+            
         }
     });
 
-    memoryBoard.appendChild(tileElement);
+    memoryGameBoard.appendChild(tileElement);
     }
-}
 
-document.getElementById('shuffle').addEventListener('click', shuffleTiles);
 
-shuffleTiles();
+document.getElementById('shuffle').addEventListener('click', resetBoard);
+
+resetBoard();
